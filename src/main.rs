6b940ee00d259f808bbb2bf9b247c6f3928dc66e
grantshandy <granthandy@proton.me>
2023-04-24 13:40:48 -0600

    use bytemuck for "safe" type casting

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use std::collections::VecDeque;
 use std::error::Error;
 use std::time::SystemTime;
 use std::borrow::{Borrow, BorrowMut};
+
 const W: usize = 800;
 const H: usize = 600;
 
@@ -13,35 +14,25 @@ const SAMPLE_RATE: f64 = 10_000.0;
 const FRAME_RATE: f64 = 30.0;
 
 struct BufferWrapper(Vec<u32>);
+
 impl Borrow<[u8]> for BufferWrapper {
     fn borrow(&self) -> &[u8] {
-        // Safe for alignment: align_of(u8) <= align_of(u32)
-        // Safe for cast: u32 can be thought of as being transparent over [u8; 4]
-        unsafe {
-            std::slice::from_raw_parts(
-                self.0.as_ptr() as *const u8,
-                self.0.len() * 4
-            )
-        }
+        bytemuck::cast_slice(&self.0)
     }
 }
+
 impl BorrowMut<[u8]> for BufferWrapper {
     fn borrow_mut(&mut self) -> &mut [u8] {
-        // Safe for alignment: align_of(u8) <= align_of(u32)
-        // Safe for cast: u32 can be thought of as being transparent over [u8; 4]
-        unsafe {
-            std::slice::from_raw_parts_mut(
-                self.0.as_mut_ptr() as *mut u8,
-                self.0.len() * 4
-            )
-        }
+        bytemuck::cast_slice_mut(&mut self.0)
     }
 }
+
 impl Borrow<[u32]> for BufferWrapper {
     fn borrow(&self) -> &[u32] {
         self.0.as_slice()
     }
 }
+
 impl BorrowMut<[u32]> for BufferWrapper {
     fn borrow_mut(&mut self) -> &mut [u32] {
         self.0.as_mut_slice()
